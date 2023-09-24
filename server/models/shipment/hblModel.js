13a756f9f672f4fd4cb56e519395a89db49ceb79
Mansab Mir <mansabmirsde@gmail.com>
2023-09-24 12:59:48 +0530

    hbl model done

--- server/models/shipment/hblModel.js
+++ server/models/shipment/hblModel.js
@@ -1,15 +1,20 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 const hblSchema = new mongoose.Schema({
   shiplineName: { type: "string", required: true },
+  mblNumber: { type: "string", required: true },
+  HBLtype: { type: "string", required: true },
+
   hblNumber: { type: "string", required: true },
   hblDate: { type: "string", required: true },
   receiptPlace: { type: "string", required: true },
   vessel: { type: "string", required: true },
+  voyage: { type: "string", required: true },
   tradeType: { type: "string", required: true },
   freightType: { type: "string", required: true },
   exchangeRate: { type: "string", required: true },
+  transhipmentPort: { type: "string", required: true },
+
   SOBdate: { type: "string", required: true },
   shippingBillNumber: { type: "string", required: true },
   shippingBillDate: { type: "string", required: true },
@@ -17,17 +22,23 @@ const hblSchema = new mongoose.Schema({
   billEntryDate: { type: "string", required: true },
   freePOL: { type: "string", required: true },
   freePOD: { type: "string", required: true },
-  containerNumber: { type: "string", required: true },
-  pkgCount: { type: "string", required: true },
-  pkgType: { type: "string", required: true },
-  grossWeight: { type: "string", required: true },
-  netWeight: { type: "string", required: true },
-  volume: { type: "string", required: true },
-  lineSeal: { type: "string", required: true },
-  shipperSeal: { type: "string", required: true },
-  customsSeal: { type: "string", required: true },
-  description: { type: "string", required: true },
   goodsType: { type: "string", required: true },
+  containerDetails: [
+    {
+      containerNumber: { type: "string", required: true },
+      containerType: { type: "string", required: true },
+      pkgCount: { type: "string", required: true },
+      pkgType: { type: "string", required: true },
+      grossWeight: { type: "string", required: true },
+      netWeight: { type: "string", required: true },
+      volume: { type: "string", required: true },
+      lineSeal: { type: "string", required: true },
+      shipperSeal: { type: "string", required: true },
+      customsSeal: { type: "string", required: true },
+      description: { type: "string", required: true },
+      hsCode: { type: String, required: true },
+    },
+  ],
 
   shipperName: {
     type: mongoose.Schema.Types.ObjectId,
@@ -53,30 +64,30 @@ const hblSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "CustomerAddress",
   },
-  deliveryAgent: {
+  agentName: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Customer",
   },
-  deliveryAddress: {
+  agentAddress: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "CustomerAddress",
   },
   loadingPort: {
     type: mongoose.Schema.Types.ObjectId,
-    ref:"Port"
+    ref: "Port",
   },
   dischargePort: {
-     type: mongoose.Schema.Types.ObjectId,
-     ref:"Port"
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Port",
   },
   deliveryPlace: {
-     type:String
+    type: String,
   },
-    etaPod: {
-      type:String
+  etaPod: {
+    type: String,
   },
 });
 
-const HBL = mongoose.model("HBL", hblSchema)
+const HBL = mongoose.model("HBL", hblSchema);
 
-module.exports= HBL
+module.exports = HBL;
