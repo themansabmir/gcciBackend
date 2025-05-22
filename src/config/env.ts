f6b829dd2d0aed1809cf367aab138827efd3a6f4
Mansab Mir <mansabmirsde@gmail.com>
2025-05-22 20:19:44 +0530

    DB string removed

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,8 +3,8 @@ const env = process.env.NODE_ENV || "development";
 // db
 const dbConnections: Record<string, string> = {
   development: "mongodb://127.0.0.1:27017/freightdex-dev",
-  staging: "mongodb://staging-db-host:27017/freightdex-staging",
-  production: "mongodb+srv://user:password@cluster.mongodb.net/freightdex-prod",
+  staging: "",
+  production: "",
 };
 export const MONGO_URI = dbConnections[env] || dbConnections.development;
 
